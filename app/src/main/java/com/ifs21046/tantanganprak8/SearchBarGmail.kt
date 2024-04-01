c91895bcc1c14f77d9817845a6251be0acc4caa5
ifs21046-itdel <riskianaomi17@gmail.com>
2024-04-01 20:30:12 +0700

    Initial commit

---
+++ app/src/main/java/com/ifs21046/tantanganprak8/SearchBarGmail.kt
@@ -0,0 +1,11 @@
+package com.ifs21046.tantanganprak8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchBarGmail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_bar_gmail)
+    }
+}
